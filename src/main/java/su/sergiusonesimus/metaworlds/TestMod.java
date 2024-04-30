92ca7f54b3e779842b816ca029660be7a2d0e582
Gordon-Frohman <sanisimov6699@gmail.com>
2024-04-30 22:33:13 +0700

    Package rename fix

--- src/main/java/su/sergiusonesimus/metaworlds/TestMod.java
+++ src/main/java/su/sergiusonesimus/metaworlds/TestMod.java
@@ -13,8 +13,8 @@ public class TestMod {
     public static final String VERSION = "1.0a";
 
     @SidedProxy(
-        clientSide = "net.tclproject.metaworlds.ClientProxy",
-        serverSide = "net.tclproject.metaworlds.ServerProxy")
+        clientSide = "su.sergiusonesimus.metaworlds.ClientProxy",
+        serverSide = "su.sergiusonesimus.metaworlds.ServerProxy")
     public static CommonProxy proxy;
 
     @EventHandler
