4405afa76c705c740863eb4ab5c9273c14eca720
Gordon-Frohman <sanisimov6699@gmail.com>
2024-10-31 03:32:37 +0700

    Optimise particles on fall algorythm

--- src/main/java/su/sergiusonesimus/metaworlds/mixin/mixins/minecraft/entity/MixinEntityLivingBase.java
+++ src/main/java/su/sergiusonesimus/metaworlds/mixin/mixins/minecraft/entity/MixinEntityLivingBase.java
@@ -4,7 +4,7 @@ import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityLivingBase;
-import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.util.AxisAlignedBB;
 import net.minecraft.util.MathHelper;
 import net.minecraft.util.Vec3;
 import net.minecraft.world.World;
@@ -15,6 +15,7 @@ import org.spongepowered.asm.mixin.Shadow;
 
 import su.sergiusonesimus.metaworlds.api.IMixinEntityLivingBase;
 import su.sergiusonesimus.metaworlds.api.IMixinWorld;
+import su.sergiusonesimus.metaworlds.api.SubWorld;
 
 @Mixin(EntityLivingBase.class)
 public abstract class MixinEntityLivingBase extends MixinEntity implements IMixinEntityLivingBase {
@@ -39,18 +40,21 @@ public abstract class MixinEntityLivingBase extends MixinEntity implements IMixi
             if (block.getMaterial() == Material.air) {
                 Vec3 globalCoords = Vec3
                     .createVectorHelper(this.posX, this.posY - 0.20000000298023224D - (double) this.yOffset, this.posZ);
-                for (World world : ((IMixinWorld) this.worldObj).getWorlds()) {
-                    Vec3 localCoords = ((IMixinWorld) world).transformToLocal(globalCoords);
-                    block = world.getBlock(
-                        MathHelper.floor_double(localCoords.xCoord),
-                        MathHelper.floor_double(localCoords.yCoord),
-                        MathHelper.floor_double(localCoords.zCoord));
-                    if (block.getMaterial() != Material.air) {
-                        targetWorld = world;
-                        i = MathHelper.floor_double(localCoords.xCoord);
-                        j = MathHelper.floor_double(localCoords.yCoord);
-                        k = MathHelper.floor_double(localCoords.zCoord);
-                        break;
+                for (World world : ((IMixinWorld) this.worldObj).getSubWorlds()) {
+                    AxisAlignedBB worldBB = ((SubWorld) world).getMaximumStretchedWorldBB(false, false);
+                    if (worldBB.intersectsWith(this.boundingBox)) {
+                        Vec3 localCoords = ((IMixinWorld) world).transformToLocal(globalCoords);
+                        block = world.getBlock(
+                            MathHelper.floor_double(localCoords.xCoord),
+                            MathHelper.floor_double(localCoords.yCoord),
+                            MathHelper.floor_double(localCoords.zCoord));
+                        if (block.getMaterial() != Material.air) {
+                            targetWorld = world;
+                            i = MathHelper.floor_double(localCoords.xCoord);
+                            j = MathHelper.floor_double(localCoords.yCoord);
+                            k = MathHelper.floor_double(localCoords.zCoord);
+                            break;
+                        }
                     }
                 }
             }
