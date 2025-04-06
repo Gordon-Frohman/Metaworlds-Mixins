21926c3229099d25571822eed4a5ba90048c6d2e
Gordon-Frohman <sanisimov6699@gmail.com>
2025-04-06 19:04:22 +0700

    Update ServerConfigurationManagerSubWorldProxy.java

--- src/main/java/su/sergiusonesimus/metaworlds/server/management/ServerConfigurationManagerSubWorldProxy.java
+++ src/main/java/su/sergiusonesimus/metaworlds/server/management/ServerConfigurationManagerSubWorldProxy.java
@@ -1,5 +1,9 @@
 package su.sergiusonesimus.metaworlds.server.management;
 
+import java.util.ArrayList;
+
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.entity.player.EntityPlayerMP;
 import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.server.management.ServerConfigurationManager;
 import net.minecraft.world.WorldServer;
@@ -16,7 +20,10 @@ public class ServerConfigurationManagerSubWorldProxy extends ServerConfiguration
         super(par1MinecraftServer);
         this.mcServerProxy = par1MinecraftServer;
         this.targetSubWorld = targetWorld;
-        this.playerEntityList = targetWorld.playerEntities;
+        this.playerEntityList = new ArrayList<EntityPlayerMP>();
+        for (EntityPlayer player : targetWorld.playerEntities) {
+            this.playerEntityList.add((EntityPlayerMP) player);
+        }
     }
 
     public NBTTagCompound getHostPlayerData() {
