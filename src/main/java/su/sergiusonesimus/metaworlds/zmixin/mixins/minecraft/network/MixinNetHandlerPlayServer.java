97e0dadd5c42af39b1f9fa3de7ee28ddc4a05c8b
Gordon-Frohman <sanisimov6699@gmail.com>
2025-04-17 01:41:17 +0700

    Game now launches with Angelica installed
    Unfortunately blocks are invisible
    But hey, that's a start

--- src/main/java/su/sergiusonesimus/metaworlds/zmixin/mixins/minecraft/network/MixinNetHandlerPlayServer.java
+++ src/main/java/su/sergiusonesimus/metaworlds/zmixin/mixins/minecraft/network/MixinNetHandlerPlayServer.java
@@ -10,35 +10,29 @@ import net.minecraft.entity.item.EntityXPOrb;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.entity.player.EntityPlayerMP;
 import net.minecraft.entity.projectile.EntityArrow;
-import net.minecraft.inventory.Slot;
-import net.minecraft.item.ItemStack;
 import net.minecraft.network.NetHandlerPlayServer;
 import net.minecraft.network.Packet;
 import net.minecraft.network.play.client.C02PacketUseEntity;
 import net.minecraft.network.play.client.C03PacketPlayer;
 import net.minecraft.network.play.client.C07PacketPlayerDigging;
-import net.minecraft.network.play.client.C08PacketPlayerBlockPlacement;
 import net.minecraft.network.play.client.C12PacketUpdateSign;
-import net.minecraft.network.play.server.S02PacketChat;
 import net.minecraft.network.play.server.S23PacketBlockChange;
-import net.minecraft.network.play.server.S2FPacketSetSlot;
 import net.minecraft.server.MinecraftServer;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.tileentity.TileEntitySign;
 import net.minecraft.util.AxisAlignedBB;
 import net.minecraft.util.ChatAllowedCharacters;
-import net.minecraft.util.ChatComponentTranslation;
-import net.minecraft.util.EnumChatFormatting;
 import net.minecraft.world.WorldServer;
-import net.minecraftforge.event.ForgeEventFactory;
-import net.minecraftforge.event.entity.player.PlayerInteractEvent;
 
 import org.apache.logging.log4j.Logger;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Overwrite;
 import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.ModifyVariable;
 
-import cpw.mods.fml.common.eventhandler.Event;
+import com.llamalad7.mixinextras.sugar.Local;
+
 import su.sergiusonesimus.metaworlds.entity.player.EntityPlayerProxy;
 import su.sergiusonesimus.metaworlds.util.OrientedBB;
 import su.sergiusonesimus.metaworlds.zmixin.interfaces.entity.IMixinEntity;
@@ -511,126 +505,14 @@ public abstract class MixinNetHandlerPlayServer {
         }
     }
 
-    /**
-     * Processes block placement and block activation (anvil, furnace, etc.)
-     */
-    @Overwrite
-    public void processPlayerBlockPlacement(C08PacketPlayerBlockPlacement p_147346_1_) {
-        WorldServer worldserver = (WorldServer) this.playerEntity.worldObj;
+    @ModifyVariable(method = "processPlayerBlockPlacement", at = @At(value = "STORE"), ordinal = 0)
+    private WorldServer modifyWorldServer(WorldServer worldServer) {
+        return (WorldServer) this.playerEntity.worldObj;
+    }
 
-        ItemStack itemstack = this.playerEntity.inventory.getCurrentItem();
-        boolean flag = false;
-        boolean placeResult = true;
-        int i = p_147346_1_.func_149576_c();
-        int j = p_147346_1_.func_149571_d();
-        int k = p_147346_1_.func_149570_e();
-        int l = p_147346_1_.func_149568_f();
-        this.playerEntity.func_143004_u();
-
-        if (p_147346_1_.func_149568_f() == 255) {
-            if (itemstack == null) {
-                return;
-            }
-
-            PlayerInteractEvent event = ForgeEventFactory
-                .onPlayerInteract(playerEntity, PlayerInteractEvent.Action.RIGHT_CLICK_AIR, 0, 0, 0, -1, worldserver);
-            if (event.useItem != Event.Result.DENY) {
-                this.playerEntity.theItemInWorldManager.tryUseItem(this.playerEntity, worldserver, itemstack);
-            }
-        } else if (p_147346_1_.func_149571_d() >= this.serverController.getBuildLimit() - 1
-            && (p_147346_1_.func_149568_f() == 1
-                || p_147346_1_.func_149571_d() >= this.serverController.getBuildLimit())) {
-                    ChatComponentTranslation chatcomponenttranslation = new ChatComponentTranslation(
-                        "build.tooHigh",
-                        new Object[] { Integer.valueOf(this.serverController.getBuildLimit()) });
-                    chatcomponenttranslation.getChatStyle()
-                        .setColor(EnumChatFormatting.RED);
-                    this.playerEntity.playerNetServerHandler.sendPacket(new S02PacketChat(chatcomponenttranslation));
-                    flag = true;
-                } else {
-                    double dist = playerEntity.theItemInWorldManager.getBlockReachDistance() + 1;
-                    dist *= Math.max(1.0d, 1.0d / ((IMixinWorld) worldserver).getScaling());
-                    dist *= dist;
-                    if (this.hasMoved
-                        && this.playerEntity.getDistanceSq((double) i + 0.5D, (double) j + 0.5D, (double) k + 0.5D)
-                            < dist
-                        && !this.serverController.isBlockProtected(worldserver, i, j, k, this.playerEntity)) {
-                        // record block place result so we can update client itemstack size if place event was
-                        // cancelled.
-                        if (!this.playerEntity.theItemInWorldManager.activateBlockOrUseItem(
-                            this.playerEntity,
-                            worldserver,
-                            itemstack,
-                            i,
-                            j,
-                            k,
-                            l,
-                            p_147346_1_.func_149573_h(),
-                            p_147346_1_.func_149569_i(),
-                            p_147346_1_.func_149575_j())) {
-                            placeResult = false;
-                        }
-                    }
-
-                    flag = true;
-                }
-
-        if (flag) {
-            this.playerEntity.playerNetServerHandler.sendPacket(new S23PacketBlockChange(i, j, k, worldserver));
-
-            if (l == 0) {
-                --j;
-            }
-
-            if (l == 1) {
-                ++j;
-            }
-
-            if (l == 2) {
-                --k;
-            }
-
-            if (l == 3) {
-                ++k;
-            }
-
-            if (l == 4) {
-                --i;
-            }
-
-            if (l == 5) {
-                ++i;
-            }
-
-            this.playerEntity.playerNetServerHandler.sendPacket(new S23PacketBlockChange(i, j, k, worldserver));
-        }
-
-        itemstack = this.playerEntity.inventory.getCurrentItem();
-
-        if (itemstack != null && itemstack.stackSize == 0) {
-            this.playerEntity.inventory.mainInventory[this.playerEntity.inventory.currentItem] = null;
-            itemstack = null;
-        }
-
-        if (itemstack == null || itemstack.getMaxItemUseDuration() == 0) {
-            this.playerEntity.isChangingQuantityOnly = true;
-            this.playerEntity.inventory.mainInventory[this.playerEntity.inventory.currentItem] = ItemStack
-                .copyItemStack(this.playerEntity.inventory.mainInventory[this.playerEntity.inventory.currentItem]);
-            Slot slot = this.playerEntity.openContainer
-                .getSlotFromInventory(this.playerEntity.inventory, this.playerEntity.inventory.currentItem);
-            this.playerEntity.openContainer.detectAndSendChanges();
-            this.playerEntity.isChangingQuantityOnly = false;
-
-            if (!ItemStack.areItemStacksEqual(this.playerEntity.inventory.getCurrentItem(), p_147346_1_.func_149574_g())
-                || !placeResult) // force client itemstack update if place event was cancelled
-            {
-                this.sendPacket(
-                    new S2FPacketSetSlot(
-                        this.playerEntity.openContainer.windowId,
-                        slot.slotNumber,
-                        this.playerEntity.inventory.getCurrentItem()));
-            }
-        }
+    @ModifyVariable(method = "processPlayerBlockPlacement", at = @At(value = "STORE"), ordinal = 0)
+    private double modifyDist(double dist, @Local WorldServer worldserver) {
+        return dist * Math.max(1.0d, 1.0d / ((IMixinWorld) worldserver).getScaling());
     }
 
     /**
